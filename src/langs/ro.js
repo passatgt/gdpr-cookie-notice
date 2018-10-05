a9f6b9d172ba4d91fd1cba2d81c487a373a6411d
Szántó Zoltán <mrbig00@gmail.com>
2018-10-05 14:38:08 +0300

    Create RO translation

---
+++ src/langs/ro.js
@@ -0,0 +1,17 @@
+//Add strings
+gdprCookieNoticeLocales.en = {
+    description: 'Utilizăm modulele cookie pentru a vă oferi o experiență mai bună de navigare, pentru a personaliza conținutul și anunțurile, pentru a oferi funcții media sociale și pentru a analiza traficul nostru. Citiți despre modul în care folosim modulele cookie și cum le puteți controla făcând clic pe Setări cookie. Sunteți de acord cu cookie-urile noastre dacă continuați să utilizați acest site web.',
+    settings: 'Setările cookie',
+    accept: 'Acceptați cookie-uri',
+    statement: 'Declarația noastră privind cookie-urile',
+    save: 'Salvează setările',
+    always_on: 'Întotdeauna pornit',
+    cookie_essential_title: 'Cookie-uri esențiale',
+    cookie_essential_desc: 'Cookie-urile necesare vă ajută să creați un site web ușor de utilizat, permițând funcții de bază cum ar fi navigarea pe pagini și accesul în zonele securizate ale site-ului web. Site-ul web nu poate funcționa corect fără aceste cookie-uri.',
+    cookie_performance_title: 'Cookie-uri de performanță',
+    cookie_performance_desc: 'Aceste cookie-uri sunt folosite pentru a îmbunătăți performanța și funcționalitatea site-urilor noastre, dar nu sunt esențiale pentru utilizarea lor. De exemplu, stochează limba dvs. preferată sau regiunea în care vă aflați.',
+    cookie_analytics_title: 'Cookie-uri de statistici',
+    cookie_analytics_desc: 'Utilizăm cookie-urile de statistici pentru a ne ajuta să măsuram modul în care utilizatorii interacționează cu conținutul site-ului, ceea ce ne ajută să personalizăm site-urile și aplicațiile noastre pentru a vă îmbunătăți experiența.',
+    cookie_marketing_title: 'Cookie-uri de marketing',
+    cookie_marketing_desc: 'Aceste module cookie sunt folosite pentru a face mesajele publicitare mai relevante pentru dvs. și interesele dvs. Intenția este de a afișa anunțuri relevante și angajate pentru utilizatorul individual și, prin urmare, mai valoroase pentru editori și pentru agenții de publicitate terță parte.'
+}
