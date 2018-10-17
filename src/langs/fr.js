a3db6928e9c5c4262378d47bb84681b6bb33686f
Romain <romain@infine-digital.be>
2018-10-17 09:36:50 +0200

    Added french translation

---
+++ src/langs/fr.js
@@ -0,0 +1,17 @@
+//Add strings
+gdprCookieNoticeLocales.fr = {
+  description: 'Nous utilisons des cookies pour améliorer votre expérience de navigation, personnaliser le contenu et les publicités, pour proposer les fonctionnalités des médias sociaux ainsi que pour analyser notre trafic. Découvrez comment nous utilisons les cookies et comment vous pouvez les contrôler en cliquant sur Paramètres des cookies.',
+  settings: 'Paramètres des cookies',
+  accept: 'J\'accepte',
+  statement: 'Politique de cookies',
+  save: 'Sauvegarder',
+  always_on: 'Toujours actif',
+  cookie_essential_title: 'Cookie technique',
+  cookie_essential_desc: 'Les cookies techniques permettent de rendre un site Web utilisable en activant des fonctions de base telles que la navigation des pages dans les zones sécurisées du site. Le site Web ne peut pas fonctionner correctement sans ces cookies.',
+  cookie_performance_title: 'Cookies de perfomance',
+  cookie_performance_desc: 'Ces cookies sont utilisés pour améliorer les performances et les fonctionnalités de notre sites Web mais ne sont pas essentiels à leur utilisation. Par exemple, il stocke votre langue préférée ou la région dans laquelle vous vous trouvez.',
+  cookie_analytics_title: 'Cookies d\'analyse',
+  cookie_analytics_desc: 'Nous utilisons des cookies d\'analyse pour nous aider à mesurer la manière dont les utilisateurs interagissent avec le contenu de notre site Web, ce qui nous aide à personnaliser celui-ci afin d\'améliorer votre expérience.',
+  cookie_marketing_title: 'Cookies Marketing',
+  cookie_marketing_desc: 'Ces cookies sont utilisés pour rendre les messages publicitaires plus pertinents pour vous et vos intérêts. L\'intention est d\'afficher des annonces pertinentes et attrayantes pour l\'utilisateur et donc plus utiles pour les éditeurs et les annonceurs tiers.'
+}
