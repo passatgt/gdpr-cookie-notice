b08d22e013c700d14af9f50598b8648d1d2f12df
Hilario Serrano <hilarioserrano@gmail.com>
2021-12-12 12:55:24 +0100

    added spanish translation

---
+++ src/langs/es.js
@@ -0,0 +1,17 @@
+//Add strings
+gdprCookieNoticeLocales.es = {
+  description: 'Utilizamos cookies propias sólo con finalidad técnica. Puedes consultar cómo gestionamos las cookies y cómo puede controlarlas haciendo clic en Configuración de cookies. Acepte las cookies haciendo clic en el botón de aceptación.',
+  settings: 'Configuración de cookies',
+  accept: 'Aceptar cookies',
+  statement: 'Política de cookies',
+  save: 'Guardar configuración',
+  always_on: 'Siempre activas',
+  cookie_essential_title: 'Cookies esenciales del sitio',
+  cookie_essential_desc: 'Las cookies necesarias ayudan a que un sitio web sea utilizable al permitir funciones básicas como la navegación por las páginas y el acceso a áreas seguras del sitio web. El sitio web no puede funcionar correctamente sin estas cookies.',
+  cookie_performance_title: 'Cookies de rendimiento',
+  cookie_performance_desc: 'Estas cookies se utilizan para mejorar el rendimiento y la funcionalidad de nuestros sitios web, pero no son esenciales para su uso. Por ejemplo, almacena su idioma preferido o la región en la que se encuentra.',
+  cookie_analytics_title: 'Cookies de analítica web',
+  cookie_analytics_desc: 'Utilizamos cookies de análisis para ayudarnos a medir cómo interactúan los usuarios con el contenido del sitio web, lo que nos ayuda a personalizar nuestros sitios web y la aplicación para usted con el fin de mejorar su experiencia.',
+  cookie_marketing_title: 'Cookies de marketing',
+  cookie_marketing_desc: 'Estas cookies se utilizan para que los mensajes publicitarios sean más relevantes para usted y sus intereses. La intención es mostrar anuncios que sean relevantes y atractivos para el usuario individual y, por lo tanto, más valiosos para los editores y los anunciantes de terceros.'
+}
